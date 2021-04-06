1ce1e0fa6592ace5d4d05bc7f1a469b459a9a563
Andres Ramirez <jor.ramirez@pascualbravo.edu.co>
2021-04-05 23:49:33 -0500

    Inicializacion de proyecto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PruebaKite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
